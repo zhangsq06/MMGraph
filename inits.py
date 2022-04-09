f74b0539511716d72791506d0a4a6374a59d0ebe
zhangsq06 <36849244+zhangsq06@users.noreply.github.com>
2022-04-09 14:35:14 +0800

    Update inits.py

--- inits.py
+++ inits.py
@@ -1,15 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 31 17:46:27 2021
+
+@author: ShuangquanZhang
+"""
+
 import numpy as np  
 import tensorflow as tf 
-
-
-
 def normal(shape, name =None):
 	n_input = shape[0]
 	n_output = shape[1]
-	tmp = tf.random_normal(shape, stddev = 0.01)
+	tmp = tf.random_normal(shape, stddev = 0.001)
 	return tf.Variable(tmp, name = name)
 
-def uniform(shape, scale = 0.05, name=None):
+def uniform(shape, scale = 0.01, name=None):
 	tmp = tf.random_uniform(shape, minval =-scale, maxval=scale, dtype=tf.float32)
 	return tf.Variable(tmp, name=name)
 def zeros(shape, name=None):
